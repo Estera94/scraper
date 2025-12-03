aaab440c154c51418f6af835a4c00a98f3dffb48
Estera <estera.maftei94@gmail.com>
2025-12-03 17:07:32 +0000

    add timeout for proxy

--- aws/setup-ssl.sh
+++ aws/setup-ssl.sh
@@ -92,6 +92,15 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
+        
+        # Timeouts for long-running requests
+        proxy_connect_timeout 60s;
+        proxy_send_timeout 60s;
+        proxy_read_timeout 60s;
+        
+        # Buffer settings
+        proxy_buffering off;
+        proxy_request_buffering off;
     }
 }
 
